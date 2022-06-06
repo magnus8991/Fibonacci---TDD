52ecd5a23c72a72ca00a4c346a17e1ccc257fcc7
magnus8991 <dalfredomandon@unicesar.edu.co>
2022-06-06 14:04:52 -0500

    Nuevas pruebas, refactorización y limpieza de código, ejecución de nuevas pruebas

--- Fibonacci/FibonacciMethod.cs
+++ Fibonacci/FibonacciMethod.cs
@@ -4,12 +4,11 @@ public class FibonacciMethod
 {
     public int GetValue(int position)
     {
-        var doublePreviousValue = 0;
         var previousValue = 0;
         var currentValue = 0;
         for (var i = 0; i <= position; i++)
         {
-            doublePreviousValue = previousValue;
+            var doublePreviousValue = previousValue;
             previousValue = currentValue;
             currentValue = i == 1 ? i : doublePreviousValue + previousValue;
         }
--- FibonacciTest/FibonacciTest.cs
+++ FibonacciTest/FibonacciTest.cs
@@ -40,4 +40,32 @@ public class FibonacciTest
         var value = FibonacciMethod.GetValue(3);
         Assert.AreEqual(2,value);
     }
+
+    [TestMethod]
+    public void WhenPositionIsFourReturnThree()
+    {
+        var value = FibonacciMethod.GetValue(4);
+        Assert.AreEqual(3,value);
+    }
+
+    [TestMethod]
+    public void WhenPositionIsFiveReturnFive()
+    {
+        var value = FibonacciMethod.GetValue(5);
+        Assert.AreEqual(5,value);
+    }
+
+    [TestMethod]
+    public void WhenPositionIsEightReturnTwentyOne()
+    {
+        var value = FibonacciMethod.GetValue(8);
+        Assert.AreEqual(21,value);
+    }
+
+    [TestMethod]
+    public void WhenPositionIsTwelveReturnOneHundredFortyFour()
+    {
+        var value = FibonacciMethod.GetValue(12);
+        Assert.AreEqual(144,value);
+    }
 }
